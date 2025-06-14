3daf4f1cad41b33e55c8e4d39ef1e9364df0cf09
Andrew <andrewklad@yandex.ru>
2025-06-15 01:05:41 +0300

    Добавлен проект с моделями; добавлена модель для пользователя

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Guid { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9]+$")]
+        public string Login { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9]+$")]
+        public string Password { get; set; }
+        public int Gender { get; set; }
+        public DateTime? Birthday { get; set; }
+        public bool Admin { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime RevokedOn { get; set; }
+        public string RevokedBy { get; set; }
+    }
+}
